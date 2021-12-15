69bd75b4d05289258470aa410bc987e7ee4bba33
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2021-12-16 00:15:47 +0900

    플레이어 스탯 구조체화

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -12,6 +12,13 @@ namespace CSharp
             Archer =2,
             Mage = 3
         }
+
+        struct Player
+        {
+            public int hp;
+            public int attack;
+            
+        }
         
         static ClassType ChooseClass()
         {
@@ -38,27 +45,27 @@ namespace CSharp
             }
             return choice;
         }
-        static void CreatePlayer(ClassType choice, out int hp, out int attack)
+        static void CreatePlayer(ClassType choice, out Player player)
         {
-            hp = 0;
-            attack = 0;
+            player.hp = 0;
+            player.attack = 0;
             switch (choice)
             {              
                 case ClassType.Knight:
-                    hp = 100;
-                    attack = 10;
+                    player.hp = 100;
+                    player.attack = 10;
                     break;
                 case ClassType.Archer:
-                    hp = 75;
-                    attack = 12;
+                    player.hp = 75;
+                    player.attack = 12;
                     break;
                 case ClassType.Mage:
-                    hp = 50;
-                    attack = 15;
+                    player.hp = 50;
+                    player.attack = 15;
                     break;            
             }
-            Console.WriteLine($"플레이어의 체력 : {hp} ");
-            Console.WriteLine($"플레이어의 공격력 : {attack}");
+            Console.WriteLine($"플레이어의 체력 : {player.hp} ");
+            Console.WriteLine($"플레이어의 공격력 : {player.attack}");
         }
         static void Main(string[] args)
         {
@@ -69,9 +76,9 @@ namespace CSharp
                 if (choice != ClassType.None)
                 {
                     //캐릭터 생성
-                    int hp;
-                    int attack;
-                    CreatePlayer(choice, out hp, out attack);
+                    Player player;
+
+                    CreatePlayer(choice, out player);
                     //필드로 가서 몬스터랑 싸움
                     break;
                 }
