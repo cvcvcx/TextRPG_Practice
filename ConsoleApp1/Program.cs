e1e8cf08a537266e27abce31f89c946e0e1aa5ce
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2021-12-15 23:11:39 +0900

    직업 선택 함수화

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -5,18 +5,46 @@ namespace CSharp
 {
     class Program
     {
+        enum ClassType 
+        {
+            None =0,
+            Knight = 1,
+            Archer =2,
+            Mage = 3
+        }
+        
+        static ClassType ChooseClass()
+        {
+            ClassType choice = ClassType.None;
+
+            Console.WriteLine("직업을 선택하세요");
+            Console.WriteLine("[1]기사");
+            Console.WriteLine("[2]궁수");
+            Console.WriteLine("[3]법사");
+
+            string input = Console.ReadLine();
+
+            switch (input)
+            {
+                case "1":
+                    choice = ClassType.Knight;
+                    break;
+                case "2":
+                    choice = ClassType.Archer;
+                    break;
+                case "3":
+                    choice = ClassType.Mage;
+                    break;
+            }
+            return choice;
+        }
         static void Main(string[] args)
         {
+            
             while (true)
             {
-                Console.WriteLine("직업을 선택하세요");
-                Console.WriteLine("[1]기사");
-                Console.WriteLine("[2]궁수");
-                Console.WriteLine("[3]법사");
-
-                string input = Console.ReadLine();
-
-                if (input == "1" || input == "2" || input == "3")
+                ClassType choice = ChooseClass();
+                if (choice != ClassType.None)
                     break;
             }
 
