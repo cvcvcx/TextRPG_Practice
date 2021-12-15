89e3139e25f793386d9778c1613ea3bfe331e7ee
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2021-12-15 23:39:05 +0900

    초이스값에 따른 플레이어 체력공격력 생성

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -38,6 +38,28 @@ namespace CSharp
             }
             return choice;
         }
+        static void CreatePlayer(ClassType choice, out int hp, out int attack)
+        {
+            hp = 0;
+            attack = 0;
+            switch (choice)
+            {              
+                case ClassType.Knight:
+                    hp = 100;
+                    attack = 10;
+                    break;
+                case ClassType.Archer:
+                    hp = 75;
+                    attack = 12;
+                    break;
+                case ClassType.Mage:
+                    hp = 50;
+                    attack = 15;
+                    break;            
+            }
+            Console.WriteLine($"플레이어의 체력 : {hp} ");
+            Console.WriteLine($"플레이어의 공격력 : {attack}");
+        }
         static void Main(string[] args)
         {
             
@@ -45,7 +67,15 @@ namespace CSharp
             {
                 ClassType choice = ChooseClass();
                 if (choice != ClassType.None)
+                {
+                    //캐릭터 생성
+                    int hp;
+                    int attack;
+                    CreatePlayer(choice, out hp, out attack);
+                    //필드로 가서 몬스터랑 싸움
                     break;
+                }
+                    
             }
 
             
