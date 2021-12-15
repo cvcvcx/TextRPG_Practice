190a331fbfc2c9d2b5fa6eaec1034a0ead337d97
cvcvcx <69139476+cvcvcx@users.noreply.github.com>
2021-12-15 22:43:38 +0900

    FirstCommit
    직업선택 텍스트 및 입력기반으로 반응

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp
+
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("직업을 선택하세요");
+                Console.WriteLine("[1]기사");
+                Console.WriteLine("[2]궁수");
+                Console.WriteLine("[3]법사");
+
+                string input = Console.ReadLine();
+
+                if (input == "1" || input == "2" || input == "3")
+                    break;
+            }
+
+            
+
+
+        }
+    }
+}
